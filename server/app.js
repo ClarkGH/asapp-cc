68f2a2331435ccf3a207f6593fc3f155391e3496
clarkgh <demolisho@hotmail.com>
2018-04-03 01:53:18 -0700

    getting data

--- server/app.js
+++ server/app.js
@@ -1,16 +1,30 @@
 const express = require('express'),
   app = express(),
-  path = require('path');
+  path = require('path'),
+  express_graphql = require('express-graphql'),
+  { buildSchema } = require('graphql');
+
+// the schema
+let schema = buildSchema(`
+  type Query {
+    message: String
+  }
+`);
+
+// the resolver for root
+let root = {
+  message: () => 'Hi mom!'
+}
 
 app.set('port', process.env.PORT || 8080);
 
 app.use(express.static('./dist/'));
 
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   rootValue: root,
-//   graphiql: true,
-// }));
+app.use('/graphql', express_graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
